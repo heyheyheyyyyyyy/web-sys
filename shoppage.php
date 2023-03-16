e31344e8429251a98ffd55af35a890c85493390a
2201931sit <122243307+2201931sit@users.noreply.github.com>
2023-03-16 13:07:24 +0800

    update

--- shoppage.php
+++ shoppage.php
@@ -16,150 +16,78 @@
 
             </div><!-- #masthead -->
             <h2 style="text-align:center">Our Products!</h2>
-            <div class="row">
-                <div class="card-group" style="display: flex; justify-content: center; align-items: center">
-                    <div class="column">
-                        <div class="card"style="width: 18rem;height: 36rem;">
-                            <img src="images/p3.jpg" alt="Eco Bottle" style="display: block; margin: auto;" width="250" 
-                                 height="250">
-                            <div class="container">
-                                <h2>$10.00</h2>
-                                <p class="title">ECO Bottle    <b>NEW!</b></p>
 
-                                <p> Insulated Eco water bottle 1000ML vacuum flask insulated stainless steel water bottles.</p>
+            <div class="column">
+                <?php
+                show();
+                if ($result->num_rows > 0) :
+                    foreach ($result as $products) :
+                        ?>
+                        <tr>
+                            <td><a href="#">
+                                    <img width='250px' height='250px' src="<?= $products['Product_image'] ?>">
+                                </a>
+                            </td>
 
-                                <p><button class="button"><a href="productpage.php">Add to cart</a></button></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="column">
-                        <div class="card"style="width: 18rem;height: 36rem;">
-                            <img src="images/waterbottle2.jpeg" alt="Eco Bottle" style="display: block; margin: auto;" width="250" 
-                                 height="250">
+                            <td><?= $products['Product_name'] ?></td>
+                            <td><?= $products['Product_desc'] ?></td>
 
-                            <div class="container">
-                                <h2>$30.00</h2>
-                                <p class="title">ECO Bottle 2   <b>NEW!</b></p>
-                                <p>Package items for four eco water bottle full insulated for $30.00 while stocks last.</p>
-
-                                <p><button class="button"><a href="productpage.php">Add to cart</a></button></p>
-                            </div>
-                        </div>
-                    </div> 
-                    <div class="column">
-                        <div class="card"style="width: 18rem; height:36rem;">
-                            <img src="images/pic1.jpg" alt="Eco Bottle"  style="display: block; margin: auto;"width="250" 
-                                 height="250">
-                            <div class="container">
-                                <h2>$9.00</h2>
-                                <p class="title">ECO Bottle 3   <b>HOT!</b></p>
-                                <p>Stainless Steel Water Bottle Insulated Cycling Gym Eco Friendly Wide Mouth.</p>
-
-                                <p><button class="button"><a href="productpage.php">Add to cart</a></button></p>
-                            </div>
-                        </div>
-                    </div> 
-                    <div class="column">
-                        <div class="card"style="width: 18rem; height:36rem;">
-                            <img src="images/c1.jpg" alt="Eco Bottle" style="display: block; margin: auto;"width="250" 
-                                 height="250">
-                            <div class="container">
-                                <h2>$7.00</h2>
-                                <p class="title">ECO Bottle 4    <b>HOT!</b></p>
-                                <p>Manufacturer Cheaper Hiking Outdoor Sport Hot and Cold Water Children School Cute Gift Glass Eco Water Bottle.</p>
-
-                                <p><button class="button"><a href="productpage.php">Add to cart</a></button></p>
-                            </div>
-                        </div>
-                    </div>  
-                    <div class="column">
-                        <div class="card"style="width: 18rem; height:36rem;">
-                            <img src="images/pic2.jpg" alt="Eco Bottle" style="display: block; margin: auto;"width="250" 
-                                 height="250">
-                            <div class="container">
-                                <h2>$11.00</h2>
-                                <p class="title">ECO Bottle 5</p>
-                                <p>Double Wall Custom Logo Eco Bottle Water standard Mouth Vacuum Insulated Drink Sport Stainless Steel Water Bottle.</p>
-
-                                <p><button class="button"><a href="productpage.php">Add to cart</a></button></p>
-                            </div>
-                        </div>
-                    </div>  
-                    <div class="column">
-                        <div class="card"style="width: 18rem; height:36rem;">
-                            <img src="images/waterbottle3.jpeg" alt="Eco Bottle" style="display: block; margin: auto;"width="250" 
-                                 height="250">
-                            <div class="container">
-                                <h2>$20.00</h2>
-                                <p class="title">ECO Bottle 6</p>
-                                <p>Manufacturer Cheaper Hiking Outdoor Sport Hot and Cold Water Children School Cute Gift Glass Eco Water Bottle.</p>
-
-                                <p><button class="button"><a href="productpage.php">Add to cart</a></button></p>
-                            </div>
-                        </div>
-                    </div>  
-                    <div class="column">
-                        <div class="card"style="width: 18rem; height:36rem;">
-                            <img src="images/b1.jpg" alt="Eco Bottle" style="display: block; margin: auto;"width="250" 
-                                 height="250">
-                            <div class="container">
-                                <h2>$17.00</h2>
-                                <p class="title">ECO Bottle 7</p>
-                                <p>Manufacturer Cheaper Hiking Outdoor Sport Hot and Cold Water Children School Cute Gift Glass Eco Water Bottle.</p>
-
-                                <p><button class="button"><a href="productpage.php">Add to cart</a></button></p>
-                            </div>
-                        </div>
-                    </div>  
-                    <div class="column">
-                        <div class="card"style="width: 18rem; height:36rem;">
-                            <img src="images/b2.jpg" alt="Eco Bottle" style="display: block; margin: auto;"width="250" 
-                                 height="250">
-                            <div class="container">
-                                <h2>$8.00</h2>
-                                <p class="title">ECO Bottle 8</p>
-                                <p>Double Wall Custom Logo Eco Bottle Water standard Mouth Vacuum Insulated Drink Sport Stainless Steel Water Bottle.</p>
-
-                                <p><button class="button"><a href="productpage.php">Add to cart</a></button></p>
-                            </div>
-                        </div>
-                    </div>  
-                    <div class="column">
-                        <div class="card"style="width: 18rem; height:36rem;">
-                            <img src="images/b3.jpg" alt="Eco Bottle" style="display: block; margin: auto;"width="250" 
-                                 height="250">
-                            <div class="container">
-                                <h2>$5.00</h2>
-                                <p class="title">ECO Bottle 9</p>
-                                <p>Manufacturer Cheaper Hiking Outdoor Sport Hot and Cold Water Children School Cute Gift Glass Eco Water Bottle.</p>
-
-                                <p><button class="button"><a href="productpage.php">Add to cart</a></button></p>
-                            </div>
-                        </div>
-                    </div>  
-                </div>
-            </div>
-        </main>
-        <hr id="about">
-        <!-- About Section -->
-        <div class="w3-container w3-padding-32 w3-center">  
-            <h3 style="text-align:center;background-color: #f2f2f2;"> About Us, The Eco Bottle </h3><br>
-
-            <img src="images/Mission.jpg" alt="Me" style="display:block;margin:auto" width="800" height="533">
-            <div class="w3-padding-32">
-                <h4 style="text-align:center"><b>Sustainability Is Our Dream!</b></h4>
-                <h6 style="text-align:center"><i>With Passion, Going Green</i></h6>
-                <p>Welcome to our eco water bottle website! We are committed to sustainability and providing eco-friendly solutions to help reduce single-use plastic waste. Our water bottles are made from high-quality materials that are durable, reusable, and recyclable.
-
-                    We believe in taking a holistic approach to sustainability, and we strive to minimize our environmental impact at every stage of our product's lifecycle. From sourcing our materials to manufacturing and distribution, we prioritize eco-friendly practices and aim to reduce waste and carbon emissions.
-
-                    Our mission is to inspire and empower individuals to make sustainable choices and create a positive impact on our planet. Join us in our journey towards a more sustainable future!</p>
+                            <td><?= $products['Product_price'] ?></td>
+                        <p><button class = "button"><a href = "productpage.php">Add to cart</a></button></p>
+                        </tr>
+                        <?php
+                    endforeach;
+                endif;
+                ?>
             </div>
         </div>
-        <hr>
+    </div>
+</div>
+</div>
+</main>
+<hr id = "about">
+<!--About Section-->
+<div class = "w3-container w3-padding-32 w3-center">
+    <h3 style = "text-align:center;background-color: #f2f2f2;"> About Us, The Eco Bottle </h3><br>
 
-        <?php
-        include "footer.inc.php";
-        ?>
-    </body>
+    <img src = "images/Mission.jpg" alt = "Me" style = "display:block;margin:auto" width = "800" height = "533">
+    <div class = "w3-padding-32">
+        <h4 style = "text-align:center"><b>Sustainability Is Our Dream!</b></h4>
+        <h6 style = "text-align:center"><i>With Passion, Going Green</i></h6>
+        <p>Welcome to our eco water bottle website!We are committed to sustainability and providing eco-friendly solutions to help reduce single-use plastic waste. Our water bottles are made from high-quality materials that are durable, reusable, and recyclable.
+
+            We believe in taking a holistic approach to sustainability, and we strive to minimize our environmental impact at every stage of our product's lifecycle. From sourcing our materials to manufacturing and distribution, we prioritize eco-friendly practices and aim to reduce waste and carbon emissions.
+
+            Our mission is to inspire and empower individuals to make sustainable choices and create a positive impact on our planet. Join us in our journey towards a more sustainable future!</p>
+    </div>
+</div>
+<hr>
+
+<?php
+include "footer.inc.php";
+?>
+</body>
 </html>
+<?php
+
+function show() {
+    global $errorMsg, $success, $result;
+// Create database connection.
+    $config = parse_ini_file('../../private/db-config.ini');
+    $conn = new mysqli($config['servername'], $config['username'],
+            $config['password'], $config['dbname']);
+// Check connection
+    if ($conn->connect_error) {
+        $errorMsg = "Connection failed: " . $conn->connect_error;
+        $success = false;
+    } else {
+// Prepare the statement:
+        $stmt = $conn->prepare("SELECT * FROM Group2.Product;");
+// Execute the query statement:
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+    }
+    $conn->close();
+}
+?>
