171ab44c4858b2d9f57bc091f3b1ae4d94a2afa9
xiner27 <xinertan278@gmail.com>
2023-03-27 02:05:51 +0800

    fixed product page

--- productpage.php
+++ productpage.php
@@ -29,7 +29,7 @@ if (isset($_POST['addCart'])) {
             $row = $result->fetch_assoc();
             $cart_id = $row["Cart_id"];
             $qty = $row["Cart_Qty"];
-           //echo "<script>console.log('before qty : {$qty}');</script>";
+            //echo "<script>console.log('before qty : {$qty}');</script>";
         }
         if (isset($qty)) {
             $stmt->close();
@@ -62,12 +62,70 @@ if (isset($_POST['addCart'])) {
     <head>
         <?php
         include "head.inc.php";
-        ?>
-    </head>
-    <body>
-        <?php
         include "nav.inc.php";
         ?>
+        <style>
+            .container {
+                margin: 0 auto;
+                max-width: 960px; /* Optional - set a max-width to prevent the container from getting too wide */
+            }
+            h1 {
+                font-size: 2.5rem;
+                margin-bottom: 30px;
+            }
+            img {
+                max-width: 100%;
+                margin-bottom: 30px;
+            }
+            p {
+                font-size: 1.2rem;
+                line-height: 1.5;
+                margin-bottom: 30px;
+            }
+            .quantity-input {
+                margin-bottom: 20px;
+            }
+            label {
+                font-weight: bold;
+                margin-right: 10px;
+            }
+            input[type="number"] {
+                width: 50px;
+            }
+            button {
+                width: 150px;
+                height: 50px;
+                background-color: blue;
+                color: white;
+                border: none;
+                border-radius: 5px;
+                font-size: 16px;
+                cursor: pointer;
+                margin: 10px;
+                padding: 10px 20px;
+                border-radius: 5px;
+            }
+            button:hover {
+                background-color: #0069d9;
+            }
+            a {
+                text-decoration: none;
+            }
+            .cart-button {
+                background-color: #28a745;
+                color: #fff;
+                padding: 10px 20px;
+                border-radius: 5px;
+                font-size: 1.2rem;
+            }
+            .cart-button:hover {
+                background-color: #218838;
+            }
+
+
+        </style>
+    </head>
+    <body>
         <main class="container">
             <?php
 // Get the product ID from the query parameter
@@ -81,24 +139,18 @@ if (isset($_POST['addCart'])) {
 // Fetch the product from the database
             $config = parse_ini_file('../../private/db-config.ini');
             $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
-
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
-
             $stmt = $conn->prepare("SELECT * FROM Group2.Product WHERE Product_id = ?");
             $stmt->bind_param("i", $product_id);
             $stmt->execute();
-
             $result = $stmt->get_result();
-
             if ($result->num_rows == 0) {
                 // Product not found
                 die("Product not found");
             }
-
             $products = $result->fetch_assoc();
-
             $stmt->close();
             $conn->close();
             ?>
@@ -109,17 +161,18 @@ if (isset($_POST['addCart'])) {
             <p>Price: $<?= $products['Product_price'] ?></p>
 
             <!-- Add to cart form -->
-            <form action="" method="POST">
+            <form action="" method="POST" style="display: inline-block;">
                 <input type="hidden" name="product_id" value="<?= $products['Product_id'] ?>"> 
-                <label for="quantity">Quantity:</label>
-                <input type="number" id="quantity" name="quantity" value="1" min="1" max="10">
-                <button class="btn btn-info" type="submit" name="addCart">Add to Cart</button>
+                <div class="quantity-input">
+                    <label for="quantity">Quantity:</label>
+                    <input type="number" id="quantity" name="quantity" value="1" min="1" max="10">
+                </div>
+                <br> <!-- add a line break to separate the form elements -->
+                <button class="btn btn-info" type="submit" name="addCart" style="display: inline-block; margin-left: 10px;">Add to Cart</button>
             </form>
 
             <!-- View Cart button -->
-            <a href="cart.php"><button>View Cart</button></a>
-
-
+            <a href="cart.php" style="display: inline-block; margin-left: 10px;"><button style="display: inline-block;">View Cart</button></a>
         </main>
         <?php
         include "footer.inc.php";
