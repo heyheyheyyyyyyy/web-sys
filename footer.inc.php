f26a0100d260b658e1836ef222acccb56f7f6d8d
ChungShing <chungshinglai@gmail.com>
2023-03-29 14:12:46 +0800

    Update footer.inc.php

--- footer.inc.php
+++ footer.inc.php
@@ -1,37 +1,20 @@
 <footer>
     <div class="footerLeft">
         <div class="footerMenu">
-            <h1 class="fMenuTitle">About Us</h1>
             <ul class="fList">
-                <li class="fListItem">Personnel</li>
-                <li class="fListItem">Contact</li>
-                <li class="fListItem">Locations</li>
-            </ul>
-        </div>
-        <div class="footerMenu">
-            <h1 class="fMenuTitle">Useful Links</h1>
-            <ul class="fList">
-                <li class="fListItem">Support</li>
-                <li class="fListItem">FAQ</li>
-                <li class="fListItem">Stories</li>
+                <li class="fListItem"><a class='nav-link' href='contactus.php'>Contact</a></li>
+                <li class="fListItem"><a class='nav-link' href='aboutus.php'>About Us</a></li>
             </ul>
         </div>
     </div>
     <div class="footerRight">
         <div class="footerRightMenu">
-            <h1 class="fMenuTitle">Subscribe to our newsletter</h1>
-            <div class="fMail">
-                <input type="text" placeholder="your@email.com" class="fInput">
-                <button class="fButton">Join!</button>
-            </div>
-        </div>
-        <div class="footerRightMenu">
             <h1 class="fMenuTitle">Follow Us</h1>
             <div class="fIcons">
-                <img src="./images/facebook.png" alt="" class="fIcon">
-                <img src="./images/twitter.png" alt="" class="fIcon">
-                <img src="./images/instagram.png" alt="" class="fIcon">
-                <img src="./images/whatsapp.png" alt="" class="fIcon">
+                <a href="https://www.facebook.com"><img src="./images/facebook.png" alt="facebook" class="fIcon"></a>
+                <a href="https://www.twitter.com"><img src="./images/twitter.png" alt="twitter" class="fIcon"></a>
+                <a href="https://www.instagram.com"><img src="./images/instagram.png" alt="instagram" class="fIcon"></a>
+                <a href="https://www.whatsapp.com"><img src="./images/whatsapp.png" alt="whatsapp" class="fIcon"></a>
             </div>
         </div>
         <div class="footerRightMenu">
