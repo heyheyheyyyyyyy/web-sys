ff9b9a38c05ffdd5c7fd13abc8881ce2c4c9d6bb
xiner27 <xinertan278@gmail.com>
2023-03-30 09:19:30 +0800

    added wishlist

--- wishlist.php
+++ wishlist.php
@@ -1,34 +1,35 @@
 
+<?php
+include 'session.php';?>
 
+<?php
+    
+    // Create database connection.
+    $config = parse_ini_file('../../private/db-config.ini');
+    $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
 
-//<?php
-//    include 'session.php';
-//    // Create database connection.
-//    $config = parse_ini_file('../../private/db-config.ini');
-//    $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
-//
-//    // Check connection
-//    if ($conn->connect_error) {
-//        $errorMsg = "Connection failed: " . $conn->connect_error;
-//        $success = false;
-//    } else {
-//        if (isset($_POST['deletewishlist'])) {
-//            $user_id = $_SESSION['User_id'];
-//            $product_id = $_POST['product_id'];
-//
-//            // Prepare the statement:
-//            $stmt = $conn->prepare("DELETE FROM Group2.Wishlist WHERE User_id = ? AND Product_id = ?");
-//            $stmt->bind_param("ii", $user_id, $product_id);
-//
-//            // Execute the query statement:
-//            $stmt->execute();
-//
-//            $stmt->close();
-//        }
-//    }
-//
-//    $conn->close();
-//?>
+    // Check connection
+    if ($conn->connect_error) {
+        $errorMsg = "Connection failed: " . $conn->connect_error;
+        $success = false;
+    } else {
+        if (isset($_POST['deletewishlist'])) {
+            $user_id = $_SESSION['User_id'];
+            $product_id = $_POST['product_id'];
+
+            // Prepare the statement:
+            $stmt = $conn->prepare("DELETE FROM Group2.Wishlist WHERE User_id = ? AND Product_id = ?");
+            $stmt->bind_param("ii", $user_id, $product_id);
+
+            // Execute the query statement:
+            $stmt->execute();
+
+            $stmt->close();
+        }
+    }
+
+    $conn->close();
+?>
 
 <html>
     <head><?php
@@ -74,29 +75,30 @@
             }
         </style>
     </head>
+    <body>
     <?php
-    include 'session.php';
+
 
 // Create database connection.
-    $config = parse_ini_file('../../private/db-config.ini');
-    $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+$config = parse_ini_file('../../private/db-config.ini');
+$conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
 
 // Check connection
-    if ($conn->connect_error) {
-        $errorMsg = "Connection failed: " . $conn->connect_error;
-        $success = false;
-    } else {
-        $user_id = $_SESSION['User_id'];
+if ($conn->connect_error) {
+    $errorMsg = "Connection failed: " . $conn->connect_error;
+    $success = false;
+} else {
+    $user_id = $_SESSION['User_id'];
 
-        // Prepare the statement:
-        $stmt = $conn->prepare("SELECT * FROM Group2.Wishlist INNER JOIN Group2.Product ON Group2.Wishlist.Product_id = Group2.Product.Product_id WHERE Group2.Wishlist.User_id = ?");
-        $stmt->bind_param("i", $user_id);
+    // Prepare the statement:
+    $stmt = $conn->prepare("SELECT * FROM Group2.Wishlist INNER JOIN Group2.Product ON Group2.Wishlist.Product_id = Group2.Product.Product_id WHERE Group2.Wishlist.User_id = ?");
+    $stmt->bind_param("i", $user_id);
 
         // Execute the query statement:
     $stmt->execute();
     $result = $stmt->get_result();
 
-     if ($result->num_rows > 0) {
+    if ($result->num_rows > 0) {
         // Display the results:
         while ($row = $result->fetch_assoc()) {
             echo "<div class='wishlist-item'>";
@@ -106,7 +108,7 @@
             echo "<div class='wishlist-item-price'>$" . $row['Product_price'] . "</div>";
             echo "<form method='post' action=''>";
             echo "<input type='hidden' name='product_id' value='" . $row['Product_id'] . "'>";
-            echo "<button type='submit' name='deletewishlist'>Delete</button>";
+            echo "<button type='submit' name='deletewishlist'>Remove</button>";
             echo "</form>";
             echo "</div>";
             echo "</div>";
@@ -117,45 +119,11 @@
 
     $stmt->close();
 }
-
 $conn->close();
+
     include "footer.inc.php";
     ?>
 </body>
 </html>
 
-//<?php
-//include 'session.php';
-//if (isset($_POST['addwishlist'])) {
-//    if (!isset($_SESSION['User_id'])) {
-//        echo '<script> alert("Please log in before you can add items to wishlist")</script>';
-//    }
-//
-//    // Create database connection.
-//    $config = parse_ini_file('../../private/db-config.ini');
-//    $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
-//
-//    // Check connection
-//    if ($conn->connect_error) {
-//        $errorMsg = "Connection failed: " . $conn->connect_error;
-//        $success = false;
-//    } else {
-//        $user_id = $_SESSION['User_id'];
-//        $product_id = $_POST['product_id'];
-//
-//        // Prepare the statement:
-//        $stmt = $conn->prepare("INSERT INTO Group2.Wishlist (User_id, Product_id) VALUES (?, ?)");
-//        $stmt->bind_param("ii", $user_id, $product_id);
-//
-//        // Execute the statement:
-//        if ($stmt->execute()) {
-//            echo "Product added to wishlist!";
-//        } else {
-//            echo "Error adding product to wishlist: " . $conn->error;
-//        }
-//
-//        $stmt->close();
-//    }
-//
-//    $conn->close();
-//?>
+
