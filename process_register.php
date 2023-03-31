8dac18294263daa2e04a1b8ff6a42cccbbd8488b
ChungShing <chungshinglai@gmail.com>
2023-03-31 19:37:43 +0800

    Fix validation issues with process_register

--- process_register.php
+++ process_register.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php
 // Start the session
 include "session.php";
@@ -5,97 +6,96 @@ include "session.php";
 $fname = $lname = $email = $pwd_hashed = $address = $errorMsg = "";
 $postcode = $phoneno = 0;
 $success = true;
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-// first NAME 
-    if (!empty($_POST["User_fname"])) {
-        $fname = sanitize_input($_POST["User_fname"]);
-    }
-    // LAST NAME 
-    if (empty($_POST["User_lname"])) {
-        $errorMsg = "Last name is required.<br>";
-        $success = false;
-    } else {
-        $lname = sanitize_input($_POST["User_lname"]);
-    }
-
-    //Email
-    if (empty($_POST["User_email"])) {
-        $errorMsg = "Email is required.<br>";
-        $success = false;
-    } else {
-        $email = sanitize_input($_POST["User_email"]);
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errorMsg = "Invalid email format. <br>";
-            $success = false;
-        }
-    }
-
-    //Address
-    $address_pattern = '/^[a-zA-Z0-9\s,.#\'-]+$/';
-    if (empty($_POST["User_address"])) {
-        $errorMsg = "Address is required.<br>";
-        $success = false;
-    } else {
-        $address = sanitize_input($_POST["User_address"]);
-        if (!preg_match($address_pattern, $address)) {
-            $errorMsg = "Invalid address format. <br>";
-            $success = false;
-        }
-    }
-
-    //postcode
-    if (empty($_POST["User_postcode"])) {
-        $errorMsg = "Postal code is required.<br>";
-        $success = false;
-    } else {
-        $postcode = $_POST["User_postcode"];
-        if (!preg_match("/^[0-9]{6}$/", $postcode)) {
-            $errorMsg = "Postal code must be exactly 6 digits.<br>";
-            $success = false;
-        }
-    }
-
-    //phone
-    if (empty($_POST["User_phone"])) {
-        $errorMsg = "Phone number is required.<br>";
-        $success = false;
-    } else {
-        $phoneno = $_POST["User_phone"];
-        if (!preg_match('/^[0-9]{8}$/', $phoneno)) {
-            $errorMsg = "Invalid phone number format. Phone number should be 8 digits.<br>";
-            $success = false;
-        }
-    }
-
-    //pwd
-    if (empty($_POST["User_password"]) || empty($_POST["User_password_cfm"])) {
-        $errorMsg .= "Password and confirmation are required.<br>";
-        $success = false;
-    } else {
-        if ($_POST["User_password"] != $_POST["User_password_cfm"]) {
-            $errorMsg = "Passwords do not match. <br>";
-            $success = false;
-        } else {
-            $pwd_hashed = password_hash($_POST["User_password"], PASSWORD_DEFAULT);
-        }
-    }
-} else {
-    echo "<h2> This page is not meant to be run directly.</h2>";
-    echo "<p> You can register at the link below: </p>";
-    echo "<a href = 'register.php'> Go to Sign Up page...</a>";
-    exit();
-}
 ?>
 
-<html>
+<html lang="en">
     <?php
     include "head.inc.php";
     ?>
     <body> 
         <?php
         include "nav.inc.php";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+// first NAME 
+            if (!empty($_POST["User_fname"])) {
+                $fname = sanitize_input($_POST["User_fname"]);
+            }
+            // LAST NAME 
+            if (empty($_POST["User_lname"])) {
+                $errorMsg = "Last name is required.<br>";
+                $success = false;
+            } else {
+                $lname = sanitize_input($_POST["User_lname"]);
+            }
+
+            //Email
+            if (empty($_POST["User_email"])) {
+                $errorMsg = "Email is required.<br>";
+                $success = false;
+            } else {
+                $email = sanitize_input($_POST["User_email"]);
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $errorMsg = "Invalid email format. <br>";
+                    $success = false;
+                }
+            }
+
+            //Address
+            $address_pattern = '/^[a-zA-Z0-9\s,.#\'-]+$/';
+            if (empty($_POST["User_address"])) {
+                $errorMsg = "Address is required.<br>";
+                $success = false;
+            } else {
+                $address = sanitize_input($_POST["User_address"]);
+                if (!preg_match($address_pattern, $address)) {
+                    $errorMsg = "Invalid address format. <br>";
+                    $success = false;
+                }
+            }
+
+            //postcode
+            if (empty($_POST["User_postcode"])) {
+                $errorMsg = "Postal code is required.<br>";
+                $success = false;
+            } else {
+                $postcode = $_POST["User_postcode"];
+                if (!preg_match("/^[0-9]{6}$/", $postcode)) {
+                    $errorMsg = "Postal code must be exactly 6 digits.<br>";
+                    $success = false;
+                }
+            }
+
+            //phone
+            if (empty($_POST["User_phone"])) {
+                $errorMsg = "Phone number is required.<br>";
+                $success = false;
+            } else {
+                $phoneno = $_POST["User_phone"];
+                if (!preg_match('/^[0-9]{8}$/', $phoneno)) {
+                    $errorMsg = "Invalid phone number format. Phone number should be 8 digits.<br>";
+                    $success = false;
+                }
+            }
+
+            //pwd
+            if (empty($_POST["User_password"]) || empty($_POST["User_password_cfm"])) {
+                $errorMsg .= "Password and confirmation are required.<br>";
+                $success = false;
+            } else {
+                if ($_POST["User_password"] != $_POST["User_password_cfm"]) {
+                    $errorMsg = "Passwords do not match. <br>";
+                    $success = false;
+                } else {
+                    $pwd_hashed = password_hash($_POST["User_password"], PASSWORD_DEFAULT);
+                }
+            }
+        } else {
+            echo "<h2> This page is not meant to be run directly.</h2>";
+            echo "<p> You can register at the link below: </p>";
+            echo "<a href = 'register.php'> Go to Sign Up page...</a>";
+            exit();
+        }
         ?> 
         <main class ="container">
             <hr> 
